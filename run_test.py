28f90901827b6673ebe9be8e02b3a1dbf25cf2b4
Jordan Justen <jordan.l.justen@intel.com>
2017-01-05 16:22:55 -0800

    run_test.py: Test 1K and 4K images

--- run_test.py
+++ run_test.py
@@ -25,153 +25,67 @@ for opt, arg in opts:
   elif opt in ("-f", "--csvfile"):
     csvfile = arg
 
-number_of_tests = 7
+def run_one_test(num_images, image_file):
+    args = ["./4K_displaywall_bench", "-width", "11520", "-height", "2160",
+            "-testmode", "fps"]
+    if num_images > 0:
+        args += ["-swap_interval", "0", "-mipmap", "true", "-layout", "true"]
+    for i in range(num_images):
+        args += ["-i", image_file]
+    if num_images == 0:
+        print 'Running with 0 images'
+    else:
+        print 'Running with', num_images, 'copies of', image_file
+    return subprocess.check_output(args, cwd=local_cwd)
+
+def read_rows():
+    rows = []
+    if os.path.isfile(csvfile):
+        with open(csvfile, 'rb') as cf:
+            reader = csv.reader(cf)
+            for row in reader:
+                rows.append(row)
+            cf.close()
+    return rows
+
+num_images = (0, 1, 2, 3, 4, 5, 10)
+
+number_of_tests = len(num_images)
 # add one row for the column label
 number_of_rows = number_of_tests + 1
 
-rows = []
-append_to_existing_data = False
-if os.path.isfile(csvfile):
-  with open(csvfile, 'rb') as cf:
-    reader = csv.reader(cf)
-    for row in reader:
-      rows.append(row)
-  cf.close()
-  if len(rows) != 0:
-    append_to_existing_data = True
+rows = read_rows()
 
-with open(csvfile, 'wb') as cf:
-  writer = csv.writer(cf)
-  column_label = "FPS " + distname + version
-  if append_to_existing_data:
+if len(rows) != number_of_rows:
+    with open(csvfile, 'wb') as cf:
+        writer = csv.writer(cf)
+        writer.writerow(["num images"])
+        for i in num_images:
+            writer.writerow([str(i)])
+        cf.close()
+    rows = read_rows()
+
+def run_tests_for_size(rows, size):
+    column_label = ' '.join(("FPS", distname, version, size))
     row = rows[0]
     row.append(column_label)
-    writer.writerow(row)
-  else:
-    writer.writerow(["num images", column_label])
 
-#basic 3x 4K viewport test
-  data = subprocess.check_output(
-           ["./4K_displaywall_bench", "-width", "11520", "-height", "2160",
-                                      "-swap_interval", "0",
-                                      "-testmode", "fps"
-                                      ], cwd=local_cwd)
-  if append_to_existing_data:
-    row = rows[1]
-    row.append(data)
-    writer.writerow(row)
-  else:
-    writer.writerow(["0", data])
+    #basic 3x 4K viewport test
+    for i in range(len(num_images)):
+        image_file = "./images/bubble" + size + ".png"
+        data = run_one_test(num_images[i], image_file)
+        row = rows[i + 1]
+        row.append(data)
 
+def write_csv(filename, rows):
+    with open(csvfile, 'wb') as cf:
+        writer = csv.writer(cf)
+        for row in rows:
+            writer.writerow(row)
+        cf.close()
 
-#1 image
-  data = subprocess.check_output(
-           ["./4K_displaywall_bench", "-width", "11520", "-height", "2160",
-                                      "-swap_interval", "0",
-                                      "-mipmap", "true", "-layout", "true",
-                                      "-testmode", "fps",
-                                      "-i", "./images/bubble4K.png",
-                                      ], cwd=local_cwd)
-  if append_to_existing_data:
-    row = rows[2]
-    row.append(data)
-    writer.writerow(row)
-  else:
-    writer.writerow(["1", data])
-
-#2 images
-  data = subprocess.check_output(
-           ["./4K_displaywall_bench", "-width", "11520", "-height", "2160",
-                                      "-swap_interval", "0",
-                                      "-mipmap", "true", "-layout", "true",
-                                      "-testmode", "fps",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      ], cwd=local_cwd)
-  if append_to_existing_data:
-    row = rows[3]
-    row.append(data)
-    writer.writerow(row)
-  else:
-    writer.writerow(["2", data])
-
-#3 images
-  data = subprocess.check_output(
-           ["./4K_displaywall_bench", "-width", "11520", "-height", "2160",
-                                      "-swap_interval", "0",
-                                      "-mipmap", "true", "-layout", "true",
-                                      "-testmode", "fps",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      ], cwd=local_cwd)
-  if append_to_existing_data:
-    row = rows[4]
-    row.append(data)
-    writer.writerow(row)
-  else:
-    writer.writerow(["3", data])
-
-#4 images
-  data = subprocess.check_output(
-           ["./4K_displaywall_bench", "-width", "11520", "-height", "2160",
-                                      "-swap_interval", "0",
-                                      "-mipmap", "true", "-layout", "true",
-                                      "-testmode", "fps",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      ], cwd=local_cwd)
-  if append_to_existing_data:
-    row = rows[5]
-    row.append(data)
-    writer.writerow(row)
-  else:
-    writer.writerow(["4", data])
-
-#5 images
-  data = subprocess.check_output(
-           ["./4K_displaywall_bench", "-width", "11520", "-height", "2160",
-                                      "-swap_interval", "0",
-                                      "-mipmap", "true", "-layout", "true",
-                                      "-testmode", "fps",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      ], cwd=local_cwd)
-  if append_to_existing_data:
-    row = rows[6]
-    row.append(data)
-    writer.writerow(row)
-  else:
-    writer.writerow(["5", data])
-
-#10 images
-  data = subprocess.check_output(
-           ["./4K_displaywall_bench", "-width", "11520", "-height", "2160",
-                                      "-swap_interval", "0",
-                                      "-mipmap", "true", "-layout", "true",
-                                      "-testmode", "fps",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      "-i", "./images/bubble4K.png",
-                                      ], cwd=local_cwd)
-  if append_to_existing_data:
-    row = rows[7]
-    row.append(data)
-    writer.writerow(row)
-  else:
-    writer.writerow(["10", data])
-
+run_tests_for_size(rows, '1K')
+run_tests_for_size(rows, '4K')
+write_csv(csvfile, rows)
 
 print csvfile, " has been written with test data."
